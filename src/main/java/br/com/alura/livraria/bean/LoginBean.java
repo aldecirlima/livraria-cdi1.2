ec00dda1b188a887d5a7c3e766b637f1d3471e08
Aldecir Neves de Lima <aldecirlima@gmail.com>
2022-07-26 19:40:47 -0300

    Aula 03, lição 03 concluída

--- src/main/java/br/com/alura/livraria/bean/LoginBean.java
+++ src/main/java/br/com/alura/livraria/bean/LoginBean.java
@@ -1,13 +1,16 @@
 package br.com.alura.livraria.bean;
 
 import java.io.Serializable;
+import java.util.Map;
 
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import br.com.alura.alura_lib.helper.MessageHelper;
+import br.com.alura.alura_lib.jsf.annotation.ScopeMap;
+import br.com.alura.alura_lib.jsf.annotation.ScopeMap.Scope;
 import br.com.alura.livraria.dao.UsuarioDao;
 import br.com.alura.livraria.modelo.Usuario;
 
@@ -16,42 +19,53 @@ import br.com.alura.livraria.modelo.Usuario;
 public class LoginBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Usuario usuario = new Usuario();
 
 //	A injeção pode ser feita diretamente acima do objeto ou no construtor da classe
 //	desde que o construtor receba o objeto injetado como parâmetro
-	
-	private UsuarioDao usuarioDao;
-	
+
+	private UsuarioDao usuarioDao;;
+
+	private MessageHelper helper;
+
+	private Map<String, Object> sessionMap;
+
 	@Inject
-	public LoginBean(UsuarioDao usuarioDao) {
+	public LoginBean(UsuarioDao usuarioDao,@ScopeMap(Scope.SESSION) Map<String, Object> sessionMap, MessageHelper helper) {
 		this.usuarioDao = usuarioDao;
+		this.sessionMap = sessionMap;
+		this.helper = helper;
 	}
 
 	public Usuario getUsuario() {
 		return usuario;
 	}
-	
+
 	public String efetuaLogin() {
 		System.out.println("fazendo login do usuario " + this.usuario.getEmail());
-		
-		FacesContext context = FacesContext.getCurrentInstance();
+
 		boolean existe = usuarioDao.existe(this.usuario);
-		if(existe ) {
-			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
+		if (existe) {
+			sessionMap.put("usuarioLogado", this.usuario);
 			return "livro?faces-redirect=true";
 		}
-		
-		context.getExternalContext().getFlash().setKeepMessages(true);
-		context.addMessage(null, new FacesMessage("Usuário não encontrado"));
-		
+
+		helper.onFlash().addMessage(new FacesMessage("Usuário não encontrado"));
+
 		return "login?faces-redirect=true";
 	}
-	
+
 	public String deslogar() {
-		FacesContext context = FacesContext.getCurrentInstance();
-		context.getExternalContext().getSessionMap().remove("usuarioLogado");
+		sessionMap.remove("usuarioLogado");
 		return "login?faces-redirect=true";
+	}
+
+	public MessageHelper getHelper() {
+		return helper;
+	}
+
+	public void setHelper(MessageHelper helper) {
+		this.helper = helper;
 	}
 }
