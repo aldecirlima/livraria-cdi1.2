e65031556970705d467f025f24e575865b5b46a2
Aldecir Neves de Lima <aldecirlima@gmail.com>
2022-07-27 18:05:24 -0300

    Aula 04 lição 02 - Alternativos

--- src/main/java/br/com/alura/livraria/tx/MeuGerenciadorDeTransacao.java
+++ src/main/java/br/com/alura/livraria/tx/MeuGerenciadorDeTransacao.java
@@ -1,38 +1,46 @@
 package br.com.alura.livraria.tx;
 
-import javax.enterprise.inject.Specializes;
+import javax.annotation.Priority;
+import javax.enterprise.inject.Alternative;
+import javax.enterprise.inject.Typed;
+import javax.inject.Inject;
+import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
+import javax.persistence.EntityManager;
 
-import br.com.alura.alura_lib.tx.TransacionadoPadrao;
+import br.com.alura.alura_lib.tx.Transacionado;
 
-@Specializes
-public class MeuGerenciadorDeTransacao extends TransacionadoPadrao {
+@Alternative
+@Priority(Interceptor.Priority.APPLICATION)
+@Typed(Transacionado.class)
+public class MeuGerenciadorDeTransacao implements Transacionado {
 
 	private static final long serialVersionUID = -8590951365580768798L;
-	
-	
+
+	@Inject
+	private EntityManager em;
+
 	@Override
 	public Object executaComTransacao(InvocationContext context) {
-		
+
 		System.out.println("Abrindo uma transação...");
 		em.getTransaction().begin();
-		
+
 		try {
 			System.out.println("Antes de executar o método interceptado...");
 			Object resultado = context.proceed();
-			
+
 			System.out.println("Antes de comitar a transação...");
 			em.getTransaction().commit();
-			
+
 			return resultado;
 		} catch (Exception e) {
-			
+
 			System.out.println("Antes de efetuar o rollback...");
 			em.getTransaction().rollback();
 			throw new RuntimeException(e);
 		}
-		
+
 	}
-	
 
 }
